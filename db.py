0609762ae323ffc3741f2c2720fd50511d5f493e
MATHEW V KARIATH <90597530+MVK2803@users.noreply.github.com>
2024-03-19 23:28:55 +0530

    feat: function to getPoll results and clearing the poll and response tables created

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 from firebase import firebase
+from datetime import date
 #Setting up the connection to the database
 firebase_url = 'https://dailybraniac-bot-default-rtdb.firebaseio.com/'
 firebase = firebase.FirebaseApplication(firebase_url, None)
+
 def addPollToDB(pollDetails):
     result = firebase.put('/polls', pollDetails["pollId"],pollDetails)
     print(result)
@@ -12,8 +14,32 @@ def getPollDetails(poll_id):
     result = firebase.get('/polls', poll_id)
     print(result)
     return result
+
 def addResponseToDB(userName,responseDetails):
     
     result = firebase.post(f'/responses/{userName}', responseDetails)
     print(result)
     return result
+
+def getPollResults():
+    result = firebase.get('/responses', None)
+    monthlyAggregates = {}
+    for i in result:
+        aggregate=0
+        for j in result[i]:
+            aggregate+=result[i][j]['userResponse']
+        
+        monthlyAggregates[i]=aggregate
+    monthlyAggregates=({k: v for k, v in sorted(monthlyAggregates.items(), key=lambda item: item[1])})
+    monthToppers=[]
+    for i in monthlyAggregates:
+        monthToppers.append(i)
+    currentMonth = date.today().strftime("%B")
+    result = firebase.put('/monthToppers', currentMonth,monthToppers)
+    return(monthlyAggregates)
+
+def clearDB():
+    clearPoll = firebase.delete('/polls', None)
+    clearResponse = firebase.delete('/responses', None)
+    
+# getPollResults()
